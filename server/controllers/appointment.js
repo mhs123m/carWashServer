e1b647284cf0439fd6ab3583fa50dbddf4b7e4bb
mhs123m <64324346+mhs123m@users.noreply.github.com>
2022-01-10 18:00:50 +0300

    user populate

--- server/controllers/appointment.js
+++ server/controllers/appointment.js
@@ -29,7 +29,8 @@ module.exports = {
             if (!ObjectId.isValid(req.params.userId)) {
                 throw { status: 400, error: `Invalid user Id` }
             }
-            const appointment = await Appointment.find().where('userId').equals(req.params.userId).populate('serviceId')
+            var appointment = await Appointment.find().where('userId').equals(req.params.userId)
+            appointment.populate('serviceId')
             res.status(200).send(appointment);
         } catch (error) {
             res.status(404).send({
