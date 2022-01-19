69fbbadbefe6bd9ce0b0b4876fbce60194a14b55
mhs123m <64324346+mhs123m@users.noreply.github.com>
2022-01-19 15:37:33 +0300

    slashhhh

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,7 +6,7 @@ userController = require('../controllers/user');
 router.get('/users', userController.index);
 
 // GET one user
-router.get('users/:userId', userController.oneUser);
+router.get('/users/:userId', userController.oneUser);
 
 // POST / register user
 router.post('/users/register', userController.create);
